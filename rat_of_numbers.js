e5c6689759c684d6e0baad541b9619663dcef090
Aashu-Dalal <aashudalal2005@gmail.com>
2025-08-28 17:59:49 +0530

    Add program to make a pattern of right angle triangle by numbers

--- rat_of_numbers.js
+++ rat_of_numbers.js
@@ -1,11 +1,13 @@
-// for right angle triangle (*)
+// for RAT of numbers
 const prompt = require("prompt-sync")()
 
-var a = Number(prompt("enter a number: "))
+var a = (prompt("enter a number: "))
 for(let i = 1; i <= a; i++){
     for(let j = 1; j <= i; j++){
-        process.stdout.write("* ")
+        process.stdout.write(j+" ")
     }
     console.log()
 }
- 
+
+
+
