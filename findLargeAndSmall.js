89ff59063d10c08fe18a5b6938cefe350ef90790
Aashu-Dalal <aashudalal2005@gmail.com>
2025-08-29 11:17:40 +0530

    Added a program to find largest and smallest element from (short-medium)array

---
+++ findLargeAndSmall.js
@@ -0,0 +1,6 @@
+let arr = [45 ,5,58,564,54,654,4];
+large = Math.max(...arr)
+small = Math.min(...arr)
+
+console.log(`The Largest number in Array is: ${large}`)
+console.log(`The Smallest number in Array is: ${small}`)
