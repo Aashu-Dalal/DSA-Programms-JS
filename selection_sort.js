52e9215d2889d106b1a0696102be3e47de7378ff
Aashu-Dalal <aashudalal2005@gmail.com>
2025-09-14 17:09:29 +0530

    Added a program to sort an array by selection sort

---
+++ selection_sort.js
@@ -0,0 +1,17 @@
+let prompt = require("prompt-sync")();
+let arr = [3, 4, 1, 2, 5];
+
+for (let i = 0; i < arr.length; i++) {
+  let min = i;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[j] < arr[min]) {
+      min = j;
+    }
+  }
+  if (min !== i) {
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+}
+console.log(arr);
