841c8eca41d685bd3b029b1db3a10b88a8090656
Aashu-Dalal <aashudalal2005@gmail.com>
2025-09-17 21:55:17 +0530

    Added a feature so that user can give input

--- factorial.js
+++ factorial.js
@@ -1,7 +1,9 @@
-let num = Number(prompt("Enter a Number: "));
-res = 1;
-
-for (let i = 1; i<=num; i++){
-    res *= i
+let prompt = require('prompt-sync')()
+let n = Number(prompt("Enter a number: "))
+function fact(n) {
+    if (n<0) return "Enter number above 0"
+    if (n === 0 || n === 1) return 1;
+    return n * fact(n - 1);
 }
-console.log(res)
+let result = fact(n)
+console.log(result); 
