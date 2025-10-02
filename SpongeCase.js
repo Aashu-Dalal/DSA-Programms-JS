3f307fbccdad477d6d10fdaf3699484f5f5e5185
Aashu-Dalal <aashudalal2005@gmail.com>
2025-10-02 20:53:26 +0530

    Added a program to convert the word to sponge case

---
+++ SpongeCase.js
@@ -0,0 +1,12 @@
+let str = "Hello"
+let newStr = ""
+
+for (let i = 0; i<str.length; i++){
+    if(i%2 == 0){
+        newStr += str[i].toUpperCase()
+    }
+    else{
+        newStr += str[i].toLowerCase()
+    }
+}
+console.log(newStr)
