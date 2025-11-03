9b162796a8c6025070214de750012e83788f9276
Aashu-Dalal <aashudalal2005@gmail.com>
2025-11-03 20:32:25 +0530

    Add function to check if all digits are the same

---
+++ allSameOrNot.js
@@ -0,0 +1,15 @@
+let num = 77777
+let input = num.toString()
+let allSame = true
+for(let i = 0; i<input.length; i++){
+    if(input[i] != input[i+1]){
+        allSame = false
+        break
+    }
+}  
+if(allSame == true ){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
