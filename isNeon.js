6ca19e4eda334f17dcfe04400f3ae7617e94cb03
Aashu-Dalal <aashudalal2005@gmail.com>
2025-11-23 21:09:10 +0530

    Implement isNeon function to check Neon numbers

---
+++ isNeon.js
@@ -0,0 +1,19 @@
+function isNeon(num) {
+    let square = num * num;
+    let sum = 0;
+
+    while (square > 0) {
+        sum += square % 10;
+        square = Math.floor(square / 10);
+    }
+
+    if (sum === num) {
+        console.log("Neon Number");
+    } else {
+        console.log("Not a Neon Number");
+    }
+}
+
+// Examples
+isNeon(9);  // Neon Number
+isNeon(7);  // Not a Neon Number
