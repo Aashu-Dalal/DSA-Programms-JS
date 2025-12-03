a4d9bb5ff5d0d11ba3409777d80958b49807f09d
Aashu-Dalal <aashudalal2005@gmail.com>
2025-12-03 18:34:43 +0530

    Added a program that prints the lower triangle of the matrix

---
+++ printLowerTriangleMatrix.js
@@ -0,0 +1,18 @@
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for(let i= 0; i<arr.length;i++){
+    let row=""
+    for(let j= 0; j<arr[0].length;j++){
+        if(j<=i){
+            row+=arr[i][j] +" "
+        }
+        else{
+            row+="  "
+        }
+    }
+    console.log(row)
+}
