0c4234da486f8da7fe7d51f91a0c541ce9a76784
Aashu-Dalal <aashudalal2005@gmail.com>
2025-12-07 21:16:03 +0530

    Add program to count even and odd numbers in matrix

---
+++ countEvenAndOddInMatrix.js
@@ -0,0 +1,21 @@
+let arr = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+];
+
+let even = 0;
+let odd = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[0].length; j++) {
+        if (arr[i][j] % 2 === 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    } 
+}
+
+console.log("Even numbers:", even);
+console.log("Odd numbers:", odd);
