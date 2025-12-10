9cc691859d1c3349a5da5688ea01581a57bd1cef
Aashu-Dalal <aashudalal2005@gmail.com>
2025-12-10 18:58:27 +0530

    Add program to count number of steps to reduce number to zero

---
+++ stepsToReduceNumtoZero.js
@@ -0,0 +1,13 @@
+let num = 15
+let count = 0
+while(num>0){
+    if(num %2 == 0){
+        num = num/2
+        count++
+    }
+    else{
+        num = num-1
+        count ++
+    }
+}
+console.log(count)
