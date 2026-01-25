19c21e1a649f49d5b6de812773aaed23be3e8d6c
Aashu-Dalal <aashudalal2005@gmail.com>
2026-01-25 21:43:17 +0530

    Implement integerSum function to sum all digits in a string

---
+++ integerSumOnly.js
@@ -0,0 +1,11 @@
+function integerSum(str){
+    let digits = str.match(/\d/g)
+    let sum = 0
+
+    for(let i = 0; i < digits.length; i++){
+        sum += Number(digits[i])
+    }
+    return sum
+}
+
+console.log(integerSum("837hdg6ebe7")) // 31
